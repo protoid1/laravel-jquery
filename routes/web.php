1aded3d9a878b3d457b5516ca73a42dcc36aaa0a
Randi Ardiansyah <randi.ardiansyah@bni-life.co.id>
2024-06-20 16:21:57 +0700

    change routing

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
-Route::get('/Home', [HomeController::class, 'showGallery']);
+// Route::get('/Home', [HomeController::class, 'showGallery']);
 
-Route::get('/pages', [PagesController::class , 'showImage']);
+Route::get('/', [PagesController::class , 'showImage']);
